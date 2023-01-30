5e527c97a97ef9365157ea1a34a0b08bc3b84387
raphahonorato <raphaahonorato@gmail.com>
2023-01-30 20:35:55 -0300

    adicionando pequenas validações no form

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,34 @@ router.get('/', function (req, res, next) {
 });
 
 
+router.get('/new', (req, res) => {
+
+  res.render('customer', { title: 'Cadastro de Clientes' });
+
+})
+
+router.post('/new', (req, res) => {
+  if (!req.body.nome)
+    return res.redirect('/new?error=O campo nome é obrigatório!')
+
+  if (!req.body.idade)
+    return res.redirect('/new?error=O campo idade é numérico')
+
+  const nome = req.body.nome;
+  const idade = parseInt(req.body.idade);
+  const cidade = req.body.cidade;
+  const uf = req.body.uf.length > 2 ? '' : req.body.uf;
+  
+  db.insertCustomer({ nome, idade, cidade, uf })
+    .then(result => {
+
+      res.redirect('/')
+
+    })
+    .catch(error => {
+      return console.log(error)
+    })
+})
+
+
 module.exports = router;
